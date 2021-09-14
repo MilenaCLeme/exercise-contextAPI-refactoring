21dea9c0b15f539fd135d636aa5e92a4c07d24bb
MilenaCLeme <milenaleme4@hotmail.com>
2021-09-14 00:28:25 -0300

    req one - está com erro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component() {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      red: false,
+      yellow: false,
+      blue: false,
+    }
+
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(paramento, nome) {
+    this.getState({
+      [nome]: paramento
+    })
+  }
+
+  render() {
+    const carro = {
+      redCar: this.state.red,
+      yellowCar: this.state.yellow,
+      blueCar: this.state.blue,
+      moveCar: this.moveCar,
+    }
+
+    return (
+      <MyContext.Provider value={ carro }>
+        <div>
+          <Cars />
+        </div>
+      </MyContext.Provider>
+    );
+
+  }
 }
 
 export default App;
